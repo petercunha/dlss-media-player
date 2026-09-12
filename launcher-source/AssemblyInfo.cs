d253546b074a2708229c6e082ff5057a1aa00d4c
Peter Cunha <petercunha@users.noreply.github.com>
2026-09-11 17:35:01 -0700

    Add launcher icon and Windows metadata

---
+++ launcher-source/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("DLSS 5 Media Player")]
+[assembly: AssemblyDescription("DLSS 5 Media Player Launcher")]
+[assembly: AssemblyCompany("DLSS 5 Media Player contributors")]
+[assembly: AssemblyProduct("DLSS 5 Media Player")]
+[assembly: AssemblyCopyright("Copyright © 2026 DLSS 5 Media Player contributors")]
+[assembly: AssemblyVersion("0.14.1.0")]
+[assembly: AssemblyFileVersion("0.14.1.0")]
